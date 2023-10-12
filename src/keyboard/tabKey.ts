eeac154b87ebd2b054ee26a7f52e901e82f3bc38
Jim <jimkernicky@gmail.com>
2023-10-12 17:13:54 -0400

    docs: Added JSDoc comment in tabKey.ts

--- src/keyboard/tabKey.ts
+++ src/keyboard/tabKey.ts
@@ -3,6 +3,9 @@ import { cursorPosition } from "../utils/cursorPosition.js";
 const textBox = document.getElementById('text-box') as HTMLTextAreaElement;
 const tab = document.getElementById("tab")!;
 
+/**
+ * TAB key: Add functionality to add 5 spaces for the Tab key
+ */
 export default function tabKey(): void {
   if (tab.classList.contains("tab")) {
     const x = cursorPosition(textBox);
